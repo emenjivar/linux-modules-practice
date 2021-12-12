69c7d13a3c2a20bef446667a55304875b8b1fec7
carlos menjivar <carlos.itca@gmail.com>
2021-12-12 12:01:05 -0600

    Read and write char* parameters

--- sysfs/multiple-params/main.c
+++ sysfs/multiple-params/main.c
@@ -5,6 +5,62 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-static int foo;
-static int bar;
-static int
+/**
+ * Files created under /sys/kernel/multiple-params
+ */
+static char background[10];
+static char *style;
+static int icon_size;
+
+static ssize_t background_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%s\n", background);
+}
+
+static ssize_t background_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	sscanf(buf, "%s", background);
+	return count;
+}
+
+static struct kobj_attribute background_attr = __ATTR(background, 0664, background_show, background_store);
+
+/**
+ * Create a group of attributes
+ */
+static struct attribute *attrs[] = {
+	&background_attr.attr,
+	NULL // Need to terminale the list of attributes
+};
+
+static struct attribute_group attr_group = {
+	.attrs = attrs
+};
+
+static struct kobject *example_kobj;
+
+static int __init example_init(void)
+{
+	int retval;
+
+	// Create the directory /sys/kernel/custom_screen/
+	example_kobj = kobject_create_and_add("custom_screen", kernel_kobj);
+
+	if (!example_kobj)
+		return -ENOMEM;
+
+	// Create files under /sys/kernel/custom/screen/
+	retval = sysfs_create_group(example_kobj, &attr_group);
+
+	return retval;
+}
+
+static void __exit example_exit(void)
+{
+	// Delete directory /sys/kernel/custom_screen/
+	kobject_put(example_kobj);
+}
+
+module_init(example_init);
+module_exit(example_exit);
+MODULE_LICENSE("GPL");
